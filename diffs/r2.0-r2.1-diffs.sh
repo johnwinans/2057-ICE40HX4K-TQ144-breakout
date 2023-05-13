8d3e2a5bb643f38996b8e1f203536c161bfcb699
John Winans <john@winans.org>
2023-05-13 14:59:16 -0500

    Add diff images of PCB and schematic

--- diffs/r2.0-r2.1-diffs.sh
+++ diffs/r2.0-r2.1-diffs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 A_TAG=r2.0
-B_TAG=
+B_TAG=HEAD
 SCRATCH=/tmp/$$-2063
+
+PROJECT=2057-ICE40HX4K-TQ144-breakout
 
 OUT_NAME=2063-Z80-${A_TAG}-${B_TAG}-delta
 
@@ -12,17 +14,17 @@ B_TMP=${SCRATCH}/${B_TAG}
 mkdir -p ${A_TMP}
 mkdir -p ${B_TMP}
 
-git show ${A_TAG}:gerbers/2063-Z80-F_Cu.gtl > ${A_TMP}/2063-Z80-F_Cu.gtl || exit 1
-git show ${A_TAG}:gerbers/2063-Z80-B_Cu.gbl > ${A_TMP}/2063-Z80-B_Cu.gbl || exit 1
-git show ${A_TAG}:2063-Z80.pdf> ${A_TMP}/2063-Z80.pdf || exit 1
+git show ${A_TAG}:gerbers/${PROJECT}-F_Cu.gtl > ${A_TMP}/${PROJECT}-F_Cu.gtl || exit 1
+git show ${A_TAG}:gerbers/${PROJECT}-B_Cu.gbl > ${A_TMP}/${PROJECT}-B_Cu.gbl || exit 1
+git show ${A_TAG}:${PROJECT}.pdf> ${A_TMP}/${PROJECT}.pdf || exit 1
 
-git show ${B_TAG}:gerbers/2063-Z80-F_Cu.gtl > ${B_TMP}/2063-Z80-F_Cu.gtl || exit 1
-git show ${B_TAG}:gerbers/2063-Z80-B_Cu.gbl > ${B_TMP}/2063-Z80-B_Cu.gbl || exit 1
-git show ${B_TAG}:2063-Z80.pdf> ${B_TMP}/2063-Z80.pdf || exit 1
+git show ${B_TAG}:gerbers/${PROJECT}-F_Cu.gtl > ${B_TMP}/${PROJECT}-F_Cu.gtl || exit 1
+git show ${B_TAG}:gerbers/${PROJECT}-B_Cu.gbl > ${B_TMP}/${PROJECT}-B_Cu.gbl || exit 1
+git show ${B_TAG}:${PROJECT}.pdf> ${B_TMP}/${PROJECT}.pdf || exit 1
 
 # Generate a .pdf showing the diffs in the schematic drawings
 
-compare ${A_TMP}/2063-Z80.pdf ${B_TMP}/2063-Z80.pdf ${OUT_NAME}-schematic.pdf
+compare ${A_TMP}/${PROJECT}.pdf ${B_TMP}/${PROJECT}.pdf ${OUT_NAME}-schematic.pdf
 
 # Generate .png files showing the diffs between the A and B PCB versions
 
